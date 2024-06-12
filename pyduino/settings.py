45ca37101b5060f5ded6103f524c15e80410837e
kodesh01 <141357071+kodesh01@users.noreply.github.com>
2024-06-12 13:54:34 +0530

    Update settings.py

--- pyduino/settings.py
+++ pyduino/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ac)l&^8=#z@_g^g+rrhe42=znr+hpka&50)(#!dy!ul4ra49&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
